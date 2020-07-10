df40070bb226a3876c7a3f0434f337c7972b6781
daix <evirtuel@gmail.com>
2020-07-10 16:21:30 +0200

    ADD tests from connector mongo

--- spec/starWars.fixture.js
+++ spec/starWars.fixture.js
@@ -1,3 +1,25 @@
+
+const DARTH_VADOR = {
+  name: 'Darth Vador',
+  gender: 'M',
+  height: 203,
+};
+const LUKE = {
+  name: 'Luke Skywalker',
+  gender: 'M',
+  height: 172,
+};
+const CHEWBACCA = {
+  name: 'Chewbacca',
+  gender: 'M',
+  height: 230,
+};
+const LEIA = {
+  name: 'Leia Organa',
+  gender: 'F',
+  height: 150,
+};
+
 // eslint-disable-next-line require-jsdoc
 const initDb = async (knex) => {
   await knex.schema.createTable('race', (table) => {
@@ -27,32 +49,25 @@ const initDb = async (knex) => {
     {
       id: '8eb82c15-4f26-4945-b568-83905b610ba9',
       raceId: 'd4f9e1f5-555a-4d3a-9cb0-2a16f22ff50b',
-      name: 'Darth Vador',
-      gender: 'M',
-      height: 203,
+      ...DARTH_VADOR,
     },
     {
       id: '034d29fc-9dd2-417c-9c5e-8b17011ee0e1',
       raceId: 'd4f9e1f5-555a-4d3a-9cb0-2a16f22ff50b',
-      name: 'Luck Skywalker',
-      gender: 'M',
-      height: 172,
+      ...LUKE,
     },
     {
       id: '28adf8e6-7508-40ea-977a-55e08302a352',
       raceId: '08fb0d7c-499b-40fe-9074-5edc1b091e0a',
-      name: 'Chewbacca',
-      gender: 'M',
-      height: 230,
+      ...CHEWBACCA,
     },
     {
       id: '73eb1c35-a7ca-4ad9-bcd7-a6f037f58471',
       raceId: 'd4f9e1f5-555a-4d3a-9cb0-2a16f22ff50b',
-      name: 'Leia Organa',
-      gender: 'F',
-      height: 150,
+      ...LEIA,
     },
   ]).into('characters');
+
 };
 
 // eslint-disable-next-line require-jsdoc
@@ -83,4 +98,8 @@ module.exports = {
   cleanDb,
   raceSchema,
   charactersSchema,
+  LUKE,
+  CHEWBACCA,
+  DARTH_VADOR,
+  LEIA,
 };
