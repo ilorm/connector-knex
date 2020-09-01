85944a5ff743987fcb02b1385df7d3c543f8c054
stombretrooper <evirtuel@gmail.com>
2020-09-01 13:21:23 +0200

    MOD load test config from start script path (to make core work with similar test suit)

--- spec/knex.js
+++ spec/knex.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 const knex = require('knex');
 
 // eslint-disable-next-line no-process-env
-const confFile = `${__dirname}/config/${process.env.NODE_ENV}.js`;
+const confFile = `${process.cwd()}/spec/config/${process.env.NODE_ENV}.js`;
 
 // eslint-disable-next-line no-sync
-const confPath = fs.existsSync(confFile) ? confFile : `${__dirname}/config/default.js`;
-
+const confPath = fs.existsSync(confFile) ? confFile : `${process.cwd()}/spec/config/default.js`;
 
 const conf = require(confPath);
 
